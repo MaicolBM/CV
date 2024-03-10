8c6c220908737a2d51b7c69aae6903545d9984ca
MaicolBM <maaicolmorenooo@gmail.com>
2024-03-09 21:29:46 -0300

    modification in styles and add information

--- data.py
+++ data.py
@@ -2,35 +2,15 @@ data = {
     "first name" : "Michael",
     "last name" : "Moreno",
     "contact" : {
-        "email" : "michael@moreno.com",
+        "email" : "maaicolmorenooo@gmail.com",
         "phone" : "3517871198"
     },
     "description" : {
-        "short description" : "pensar descripcion corta",
-        "long description" : "pensar descripcion larga"
-    },
-    "work experience" : {
-        "Work1" : {
-            "time period" : "2018-2020",
-            "description" : "sdasdasdsad"
-        },
-        "Work2" : {
-            "time period" : "2018-2020",
-            "description" : "sdasdasdsad"
-        }
-    },
-    "education" : {
-        "bachelor" : {
-            "University" : "Universidad de Rio Cuarto",
-            "Title" : "Systems Analyst",
-            "Period" : "2020 - 2023"
-        },
-        "languages" : {
-            "English" : "B1",
-            "Spanish" : "Native",
-        }   
+        "short description" : "Developer in progress",
+        "long description" : "Hi, my name is Michael, I am a computer analyst and computer technician. My goal as a developer is to become a full-stack senior and be part of many projects."
+
     },
     "images" : {
-        "profile picture" : "profile.png"
+        "profile picture" : "profile.jpg"
     }
 }
